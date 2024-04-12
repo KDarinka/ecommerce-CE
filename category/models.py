3ce3d2aa146708195d4e0e9bb07cf43f7542f8c9
KDarinka <al059906@uacam.mx>
2024-04-12 13:26:56 -0600

    Este es mi primer paquete commit de mi proyecto

---
+++ category/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    slug = models.CharField(max_length=100, unique=True)
+    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        """
+        Devuelve la URL absoluta de la categoría.
+        """
+        return reverse('products_by_category', args=[self.slug])
+
+    def __str__(self):
+        """
+        Devuelve una representación de cadena de la categoría.
+        """
+        return self.category_name
