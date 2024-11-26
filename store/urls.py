8c95adaf7216c7c97afc7cd0a83547349894d471
KDarinka <79330600+KDarinka@users.noreply.github.com>
2024-11-26 16:13:31 -0600

    Add files via upload

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name="store"),
+    path('', views.store, name='store'),
+    path('search/', views.search, name='search'),  # Mueve esta línea hacia arriba para priorizar la búsqueda
     path('<slug:category_slug>/', views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('<slug:category_slug>/<slug:product_slug>', views.product_detail, name='product_detail'),
 ]
